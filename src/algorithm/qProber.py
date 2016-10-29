4d064f63082fcf970a1d3fef3e98bdcf265ee3c0
niha-p <niharika.purbey@gmail.com>
2016-10-28 22:43:15 -0400

    Part 1 - QPRober Done

--- src/algorithm/qProber.py
+++ src/algorithm/qProber.py
@@ -1,19 +1,35 @@
 from src.functions.add_qterms import *
 from src.glob.scheme import *
+from src.classes.given import *
 
 classification = []
-base = root.name
 count = 0 
 
-def classify(base,label,tec,tes):
-	if label.cov > tec and label.spec > tes:
-		base = base, "/", label.name
-		for child in label.child:
-			display(child)
-			classify(base,child,tec,tes)
-			base = root.name
-		classification.append(base)		 
-		
+#tec = 12000
+#tes = 0.02
+th = threshold(70000, 0.0001)
+
+#Check Threshold!!!!
+tec = th.tec
+tes = th.tes
+
+# def classify(label,tec,tes):
+def classify(label):
+	if label.cov >= tec and label.spec >= tes:
+		if label.ch_count <= 0:
+			classification.append(label.path)			
+			label.parent.flag = 3
+		else:
+			for child in label.child:
+				display(child)
+				classify(child)
+
+	elif  label.parent.flag != 3:
+		if label.parent.flag != label.parent.ch_count-1:
+			label.parent.flag += 1
+		else:
+			classification.append(label.parent.path)		
+
 	'''	
 	for child in label.child:
 		#count += 1

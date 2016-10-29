8d443a830ac42b5811a82d727241bc104e684155
niha-p <niharika.purbey@gmail.com>
2016-10-28 22:43:15 -0400

    Part 1 - QPRober Done

--- src/glob/scheme.py
+++ src/glob/scheme.py
@@ -11,15 +11,16 @@ diseases = Label("Diseases", 0, 0)
 hardware = Label("Hardware", 0, 0)
 programming = Label("Programming", 0, 0)
 
+sports.link_parent(root)
+health.link_parent(root)
+computers.link_parent(root)
 programming.link_parent(computers)
 hardware.link_parent(computers)
 diseases.link_parent(health)
 fitness.link_parent(health)
 soccer.link_parent(sports)
 basketball.link_parent(sports)
-sports.link_parent(root)
-health.link_parent(root)
-computers.link_parent(root)
+
 computers.display()
 root.display()
 d = { "Programming" : programming,
@@ -32,3 +33,11 @@ d = { "Programming" : programming,
 		"Health"	: health,
 		"Computers"	: computers,
 }
+
+def assign_lvl_coverage():
+
+	Label.lvl_list.append(0) 
+	Label.lvl_list.append(root.total_cov)
+	Label.lvl_list.append(0) 
+	for child in root.child:
+		Label.lvl_list[2] += child.total_cov

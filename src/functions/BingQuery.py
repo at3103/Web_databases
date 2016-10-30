3430a75da02e409828b9727880436059272fb8bf
niha-p <niharika.purbey@gmail.com>
2016-10-29 21:23:57 -0400

    Starting part 2

--- src/functions/BingQuery.py
+++ src/functions/BingQuery.py
@@ -7,7 +7,7 @@ def ping(site,query,key):
 	bingUrl = 'https://api.datamarket.azure.com/Data.ashx/Bing/SearchWeb/v1/Composite?Query=%27site%3a'+ site +'%20'+ query +'%27&$top=10&$format=json'
 	#Provide your account key here
 	accountKey = key
-
+	top4=[]
 	accountKeyEnc = base64.b64encode(accountKey + ':' + accountKey)
 	headers = {'Authorization': 'Basic ' + accountKeyEnc}
 	req = urllib2.Request(bingUrl, headers = headers)
@@ -16,8 +16,30 @@ def ping(site,query,key):
 	#content contains the xml/json response from Bing. 
 	cont = json.loads(content)
 #	print cont['d']['results'][0]['WebTotal']
+	print cont['d']['results'][0]['Url']
+	for i in range(4):
+		top4.append(cont['d']['results'][i]['Url'])
 	return cont['d']['results'][0]['WebTotal']
+'''
+def ping_top4(site,query,key):
 
+	bingUrl = 'https://api.datamarket.azure.com/Data.ashx/Bing/SearchWeb/v1/Composite?Query=%27site%3a'+ site +'%20'+ query +'%27&$top=10&$format=json'
+	#Provide your account key here
+	accountKey = key
+	top4 = []
+	accountKeyEnc = base64.b64encode(accountKey + ':' + accountKey)
+	headers = {'Authorization': 'Basic ' + accountKeyEnc}
+	req = urllib2.Request(bingUrl, headers = headers)
+	response = urllib2.urlopen(req)
+	content = response.read()
+	#content contains the xml/json response from Bing. 
+	cont = json.loads(content)
+#	print cont['d']['results'][0]['WebTotal']
+	print cont['d']['results'][0]['Url']
+	for i in range(4):
+		top4.append(cont['d']['results'][i]['Url'])
+	return top4
+'''
 #key = 'bing_acct_key'
 
 #print ping('fifa.com','boardfsb',key)

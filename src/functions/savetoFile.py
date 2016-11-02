229e2f34184df6c52f3a86f848c0c4e22dbb101b
niha-p <niharika.purbey@gmail.com>
2016-11-02 19:48:20 -0400

    Comments and cleaning

--- src/functions/savetoFile.py
+++ src/functions/savetoFile.py
@@ -2,15 +2,10 @@ from .. classes.classification import *
 
 def savetoFile(node, content_summary,host):
 	filename = node.name + "-" + host + ".txt"
-	#filename = node + "-" + host + ".txt"
-
 	f = open(filename, 'w')
 
 	for i in range(0,len(content_summary)):
 		line = str(content_summary[i][0])+"#" + str(float(content_summary[i][1])) + "\n"
 		line = str(line)
 		f.write(line)
-
 	f.close()	
-
-# savetoFile("root",[('the',3),('a',2)],"fifa.com")	
